7d2d37ee45e0cbc100ed1f5e48fbf45b5e3a54d9
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-05 17:50:45 +0100

    Save gameList in store

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,32 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import GameList from '@/class/GameList.class';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+    state: {
+        gameList: (null as GameList|null),
+    },
+    getters: {
+        gameList: (state) => {
+            return state.gameList;
+        },
+    },
+    mutations: {
+        initGameList(state) {
+            if (!state.gameList) {
+                state.gameList = new GameList();
+                state.gameList.initCategories('./config/categories.json');
+                state.gameList.initGames('./games');
+            }
+        },
+        reloadGameList(state) {
+            if (state.gameList) {
+                state.gameList.initCategories('./config/categories.json');
+                state.gameList.initGames('./games');
+            }
+        },
+    },
+    actions: {},
 });
