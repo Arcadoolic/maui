d9df8107e4a8020f06c06470f26b52c17b0176d1
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-05 18:35:44 +0100

    Add vertical move

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <nav :class="{hovered: verticalSelect == 0}">
+            <router-link to="/">
+                <p>Home</p>
+            </router-link>
+            <router-link to="/">
+                <p>Search</p>
+            </router-link>
+            <router-link to="/">
+                <p>Options</p>
+            </router-link>
+        </nav>
+        <router-view :verticalSelect="verticalSelect" @blockVerticalSelect="setBlockVerticalSelect"></router-view>
     </div>
 </template>
 
@@ -8,23 +19,40 @@
     import {Component, Vue} from 'vue-property-decorator';
 
     @Component
-    export default class Home extends Vue {
+    export default class App extends Vue {
+        protected verticalSelect: number = 0;
+        protected blockVerticalSelect: boolean = false;
+        protected maxVerticalSelect: number = 2;
+
         public created() {
             this.$store.commit('initGameList');
 
-            // window.addEventListener('keydown', (e) => {
-            //     if (e.code === 'ArrowLeft') {
-            //         if (this.categorySelected - 1 >= 0) {
-            //             this.categorySelected--;
-            //         } else {
-            //             this.categorySelected = this.gameList.getCategories().length - 1;
-            //         }
-            //     } else if (e.code === 'ArrowRight') {
-            //         this.categorySelected = (this.categorySelected + 1
-            //         >= this.gameList.getCategories().length) ? 0 : this.categorySelected + 1;
-            //     }
-            // });
+            window.addEventListener('keyup', (e) => {
 
+                if (e.code === 'ArrowUp') {
+                    if (!this.blockVerticalSelect) {
+                        this.verticalSelect--;
+                        if (this.verticalSelect < 0) {
+                            this.verticalSelect = this.maxVerticalSelect;
+                        }
+                    }
+                } else if (e.code === 'ArrowDown') {
+                    if (!this.blockVerticalSelect) {
+                        this.verticalSelect++;
+                        if (this.verticalSelect > this.maxVerticalSelect) {
+                            this.verticalSelect = 0;
+                        }
+                    }
+                }
+            });
+        }
+
+        /**
+         *
+         * @param val
+         */
+        protected setBlockVerticalSelect(val: boolean) {
+            this.blockVerticalSelect = val;
         }
     }
 </script>
@@ -32,6 +60,26 @@
 <style>
     body {
         background-image: url(./assets/background.jpg);
-        color: white
+        color: white;
+        margin: 0;
+        padding: 0;
     }
+
+    nav {
+        width: 100%;
+        height: 20px;
+        background: red;
+    }
+    nav.hovered {
+        height: 60px;
+    }
+        nav p {
+            padding: 0;
+            margin: 0;
+            display: none;
+        }
+
+        nav.hovered p {
+            display: inline-block;
+        }
 </style>
