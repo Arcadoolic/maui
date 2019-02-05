e2f15caeb21732aa6d59ffe056ab2d0b5fc4d7b5
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-05 14:07:07 +0100

    Easiest use of gameCategories but ugly code

--- src/class/GameList.class.ts
+++ src/class/GameList.class.ts
@@ -1,9 +1,11 @@
 import GameCategory from './GameCategory.class';
 import Game from './Game.class';
-import {existsSync} from 'fs';
+import {readFileSync, existsSync, readdirSync} from 'fs';
+import {extname} from 'path';
 
 export default class GameList {
-    protected categories: { [id: string]: GameCategory } = {};
+    protected categories: GameCategory[] = [];
+    protected categoriesById: { [id: string]: GameCategory } = {};
     protected games: Game[] = [];
 
     /**
@@ -11,12 +13,35 @@ export default class GameList {
      * @param categoriesJsonPath
      */
     public initCategories(categoriesJsonPath: string) {
+        this.categories = [];
+        this.categoriesById = {}
         if (!existsSync(categoriesJsonPath)) {
             throw new Error('`categories.json` file not found');
         }
-        const categoriesJson: GameCategoryJSON[] = require(categoriesJsonPath);
+        const categoriesJson: GameCategoryJSON[] = JSON.parse(readFileSync(categoriesJsonPath, 'utf8'));
+        this.categoriesById.all = new GameCategory({ name: 'All', id: 'all'});
+        this.categories.push(this.categoriesById.all);
         for (const category of categoriesJson) {
-            this.categories[category.id] = new GameCategory(category);
+            this.categoriesById[category.id] = new GameCategory(category);
+            this.categories.push(this.categoriesById[category.id]);
+        }
+    }
+
+    /**
+     *
+     * @param gamesPath
+     */
+    public initGames(gamesPath: string) {
+        this.games = [];
+        if (!existsSync(gamesPath)) {
+            throw new Error(gamesPath + ' not founnd');
+        }
+        const dir = readdirSync(gamesPath, 'utf8');
+        for (const file of dir) {
+            if (extname(file).toLowerCase() !== '.json') {
+                continue;
+            }
+            this.addGame(JSON.parse(readFileSync(gamesPath + '/' + file, 'utf8')));
         }
     }
 
@@ -28,9 +53,10 @@ export default class GameList {
         const game = new Game(gameJson, './abc.rom');
         // Categories
         for (const categoryId of gameJson.categories) {
-            if (this.categories[categoryId]) {
-                game.addCategory(this.categories[categoryId]);
-                this.categories[categoryId].addGame(game);
+            this.categoriesById['all'].addGame(game);
+            if (this.categoriesById[categoryId]) {
+                game.addCategory(this.categoriesById[categoryId]);
+                this.categoriesById[categoryId].addGame(game);
             }
         }
         this.games.push(game);
@@ -41,5 +67,12 @@ export default class GameList {
      */
     public getGames() {
         return this.games;
+    }
+
+    /**
+     * @return GameCategory[]
+     */
+    public getCategories() {
+        return this.categories;
     }
 }
