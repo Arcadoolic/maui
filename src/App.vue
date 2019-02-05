650c910ab6ee9e2267e088313b8ab8bc04829664
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-05 14:06:01 +0100

    Function to refresh gameList and start moving in menu

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,104 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div class="categories">
+            <div class="container">
+                <ul>
+                    <li v-for="(category, index) in gameList.getCategories()"
+                        :class="{selected: categorySelected === index}"
+                    >{{category.name}}</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <ul>
+            <li v-for="game in gameFromCurrentCategory">{{game.fullname}}</li>
+        </ul>
+
+        <button @click.prevent="refreshGame()">Refresh game</button>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import fs from 'fs';
+    import GameList from './class/GameList.class';
+    import GameCategory from '@/class/GameCategory.class';
+
+    @Component
+    export default class Home extends Vue {
+        protected gameList = new GameList();
+        protected categorySelected = 0;
+
+        @Prop() private msg!: string;
+
+        public created() {
+            this.refreshGame();
+
+            window.addEventListener('keydown', (e) => {
+                if (e.code === 'ArrowLeft') {
+                    if (this.categorySelected - 1 >= 0) {
+                        this.categorySelected--;
+                    } else {
+                        this.categorySelected = this.gameList.getCategories().length - 1;
+                    }
+                } else if (e.code === 'ArrowRight') {
+                    this.categorySelected = (this.categorySelected + 1 >= this.gameList.getCategories().length) ? 0 : this.categorySelected + 1;
+                }
+            });
+
+        }
+
+        /**
+         * Refresh categories and game list
+         */
+        public refreshGame() {
+            this.gameList.initCategories('./config/categories.json');
+            this.gameList.initGames('./games');
+            this.categorySelected = 0;
+        }
+
+        public get gameFromCurrentCategory()
+        {
+            return this.gameList.getCategories()[this.categorySelected].getGames();
+        }
+    }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+    body { background-image: url(./assets/background.jpg); color: white }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    .categories {
+        display: block;
+    }
+    .categories:after {
+        content: '';
+        display: block;
+        clear: both;
+    }
+    .categories .container {
+        position: relative;
+        text-align: center;
+        overflow: hidden;
+        height: 30px;
+        margin: 0 auto;
+    }
+    .categories ul {
+        width: 10000px;
+        position: absolute;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+        .categories ul li {
+            display: inline-block;
+            height: 30px;
+            float: left;
+            margin: 0 10px;
+            padding: 0;
+        }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+            .categories ul li.selected {
+                font-weight: bold;
+                color: red;
+            }
 </style>
