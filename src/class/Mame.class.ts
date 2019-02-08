e3fd94c691907cd600552cd90c61126833fc95d1
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-08 14:09:11 +0100

    Use execFile and use SIGQUIT to kill

--- src/class/Mame.class.ts
+++ src/class/Mame.class.ts
@@ -1,4 +1,4 @@
-import {exec, ChildProcess} from 'child_process';
+import {execFile, ChildProcess} from 'child_process';
 import Game from '@/class/Game.class';
 
 export default class Mame {
@@ -9,25 +9,40 @@ export default class Mame {
             return false;
         }
 
-        if (this.process) {
-            console.log('KILL');
-            console.log(this.process.kill('SIGTERM'));
-        }
-
+        this.stop();
         if (!this.process) {
-            console.log('exec');
-            this.process = exec('mame ' + game.romName + ' -autoboot_delay 0 -nomax -w', {killSignal: 'SIGKILL'},
+            this.process = execFile('/usr/games/mame', [game.romName, '-nomax', '-w'], {killSignal: 'SIGQUIT'},
                 (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-                console.log(`stderr: ${stderr}`);
-            });
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                    console.log(`stderr: ${stderr}`);
+                });
             this.process.on('close', (e) => {
-                console.log('close');
+                console.log('CLOSE');
+                this.process = undefined;
             });
         }
+    }
+
+    public stop() {
+        if (this.process) {
+            this.process.kill('SIGQUIT');
+            console.log(this.process);
+            if (this.process.killed) {
+                this.process = undefined;
+            } else {
+                console.error('Not killed');
+            }
+        }
+    }
+
+    /**
+     * @return boolean
+     */
+    public get isGameOn() {
+        return this.process;
     }
 }
