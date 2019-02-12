c23f678ec57a489491c0e862a90b2835085a83e6
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-12 13:56:36 +0100

    Mame::close return a promise

--- src/class/Mame.class.ts
+++ src/class/Mame.class.ts
@@ -4,40 +4,47 @@ import Game from '@/class/Game.class';
 export default class Mame {
     protected process?: ChildProcess;
 
+    /**
+     * Start a mame game, if a process is already on, kill it
+     * @param game
+     */
     public start(game: Game) {
         if (!game.romName) {
             return false;
         }
 
-        this.stop();
-        // FIXME - To improve
-        setTimeout(() => {
-            this.process = execFile('/usr/games/mame', [game.romName!, '-nomax', '-w'], {killSignal: 'SIGQUIT'},
-                (error, stdout, stderr) => {
-                    if (error) {
-                        console.error(`exec error: ${error}`);
-                        return;
-                    }
-                    console.log(`stdout: ${stdout}`);
-                    console.log(`stderr: ${stderr}`);
+        this.stop().then(
+            () => {
+                this.process = execFile('/usr/games/mame', [game.romName!, '-nomax', '-w'], {killSignal: 'SIGQUIT'},
+                    (error, stdout, stderr) => {
+                        if (error) {
+                            console.error(`exec error: ${error}`);
+                            return;
+                        }
+                        console.log(`stdout: ${stdout}`);
+                        console.log(`stderr: ${stderr}`);
+                    });
+                this.process.on('close', (e) => {
+                    console.log('CLOSE');
+                    this.process = undefined;
                 });
-            this.process.on('close', (e) => {
-                console.log('CLOSE');
-                this.process = undefined;
-            });
-        }, 500);
+        });
     }
 
-    public stop() {
-        if (this.process) {
+
+    /**
+     * Stop mame process
+     */
+    public stop(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!this.process) {
+                return resolve();
+            }
             this.process.kill('SIGQUIT');
-            // console.log(this.process);
-            // if (this.process.killed) {
-            //     this.process = undefined;
-            // } else {
-            //     console.error('Not killed');
-            // }
-        }
+            this.process.on('exit', (e) => {
+                return resolve();
+            });
+        });
     }
 
     /**
