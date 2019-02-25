a73b1a8e89e8ed315346908196d017105c16c01f
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-25 22:46:39 +0100

    Methods to load and fetch config

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import GameList from '@/class/GameList.class';
+import Config from '@/class/Config.class';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
+        config: (null as Config|null),
         gameList: (null as GameList|null),
     },
     getters: {
         gameList: (state) => {
             return state.gameList;
         },
+        config: (state) => {
+            return state.config;
+        }
     },
     mutations: {
+        initConfig(state) {
+            state.config = new Config();
+        },
         initGameList(state) {
             if (!state.gameList) {
                 state.gameList = new GameList();
