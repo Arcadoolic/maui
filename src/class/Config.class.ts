97c9e00470954135c474f164cf0d15641ff093b9
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-26 18:31:09 +0100

    Config class only load config file

--- src/class/Config.class.ts
+++ src/class/Config.class.ts
@@ -5,25 +5,14 @@ export default class Config {
 
     protected _configLoaded: boolean = false;
     protected _mameIniPath?: string;
-    protected _mameIni: {[key:string]: string|number|string[]|number[]} = {};
+    protected _mameIni: {[key: string]: string|number|string[]|number[]} = {};
     protected _gamesJsonPath?: string;
 
-    public constructor() {
-        this.load();
-    }
-
-    public get isConfigLoaded() {
-        return this._configLoaded;
-    }
-
-    public load() {
+    public load(): boolean {
         if (existsSync('./config.json')) {
             const configFile = JSON.parse(readFileSync('./config.json', 'utf8'));
             this._mameIniPath = configFile.mameIniPath;
-            if (!this.loadMameIni()) {
-                throw new Error('Load of mame.ini failed. Given path ' + this._mameIniPath);
-            }
-                this._gamesJsonPath = configFile.gamesJsonPath;
+            this._gamesJsonPath = configFile.gamesJsonPath;
             this._configLoaded = true;
             return true;
         }
@@ -34,23 +23,10 @@ export default class Config {
         // Ici on ecrit le ficher de config
     }
 
-    public loadMameIni() {
-        if (!this._mameIniPath || !existsSync(this._mameIniPath)) {
-            return false;
-        }
-        const regex = new RegExp(/^([a-z_]+)\s+(.+)$/);
-        const file = readFileSync(this._mameIniPath, 'utf8').split('\n');
-        file.forEach((line) => {
-            if (line[0] === '#') { // Skip comments
-                return true;
-            }
-            const data = regex.exec(line);
-            if (data) {
-                const value = data[2].split(';');
-                this._mameIni[data[1]] = (value.length > 1) ? value : value[0];
-            }
-
-        });
-        return true;
+    /**
+     * @return string
+     */
+    public get mameIniPath(): string {
+        return this._mameIniPath!;
     }
 }
