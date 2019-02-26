2e0c008adcaabd5d6517f08803903e50c13fe7c7
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-26 18:31:39 +0100

    Store Mame and some other things

--- src/store.ts
+++ src/store.ts
@@ -2,26 +2,28 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import GameList from '@/class/GameList.class';
 import Config from '@/class/Config.class';
+import Mame from '@/class/Mame.class';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        config: (null as Config|null),
+        config: new Config(),
         gameList: (null as GameList|null),
+        mame: new Mame(),
     },
     getters: {
         gameList: (state) => {
             return state.gameList;
         },
-        config: (state) => {
+        config: (state): Config => {
             return state.config;
-        }
+        },
+        mame: (state): Mame => {
+            return state.mame;
+        },
     },
     mutations: {
-        initConfig(state) {
-            state.config = new Config();
-        },
         initGameList(state) {
             if (!state.gameList) {
                 state.gameList = new GameList();
