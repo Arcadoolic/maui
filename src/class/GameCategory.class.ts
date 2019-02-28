2e970bb0b1ef003048583c2d9f95c48ea886b83d
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-02-28 15:15:51 +0100

    Remove id attribute from gamecategory

--- src/class/GameCategory.class.ts
+++ src/class/GameCategory.class.ts
@@ -1,13 +1,11 @@
 import Game from './Game.class';
 
 export default class GameCategory {
-    protected id: string;
-    protected name: string;
+        protected name: string;
     protected games: Game[] = [];
 
-    public constructor(gameCategoryData: GameCategoryJSON) {
-        this.id = gameCategoryData.id;
-        this.name = gameCategoryData.name;
+    public constructor(name: string) {
+        this.name = name;
     }
 
     /**
