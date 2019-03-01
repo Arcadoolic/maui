4443068eb505f42f48f9c7f2580abe3c47a14779
Tristan PAYEN <tristan.payen@infopro-digital.com>
2019-03-01 14:35:51 +0100

    Use Games component and clean code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,116 +5,58 @@
             <small>({{selectedGame.year}}, {{selectedGame.nplayerString}})</small>
         </div>
 
-        <div class="gamesContainer">
-            <div class="selectedGameBackground"></div>
-            <div class="games">
-                <ul ref="gameList">
-                    <li v-for="(game, index) in gameFromCurrentCategory" :class="{selected: selectedGameId == index}">
-                        <div class="marquee"
-                             :style="{marginLeft: Math.max(9 - Math.abs(selectedGameId - index), 0) + '%'}"
-                        ></div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <Categories :class="{hovered: verticalSelect === 2}"></Categories>
+        <Games :selectedCategory="selectedCategory" @gameChange="gameChange"></Games>
+        <Categories @categoryChange="categoryChange"></Categories>
     </span>
-
-
-        <!--<ul :class="{hovered: verticalSelect === 1}" class="games">-->
-            <!--<li v-for="(game, index) in gameFromCurrentCategory"-->
-                <!--:class="{selected: gameSelected === index}">{{game.fullname}}-->
-            <!--</li>-->
-        <!--</ul>-->
-
-        <!--<button v-if="mame.isGameOn" @click.prevent="mame.stop()">Kill</button>-->
+    <!--<button v-if="mame.isGameOn" @click.prevent="mame.stop()">Kill</button>-->
 
 </template>
 
 <script lang="ts">
 import {Vue, Component, Prop, Watch} from 'vue-property-decorator';
-import GameList from '../class/GameList.class';
+import GameList from '@/class/GameList.class';
 import Categories from '@/components/Categories.vue';
 import Mame from '@/class/Mame.class';
+import GameCategory from '@/class/GameCategory.class';
+import Games from '@/components/Games.vue';
+import Game from '@/class/Game.class';
 
 @Component({
     components: {
         Categories,
+        Games,
     },
 })
 export default class Home extends Vue {
-    protected blockVerticalSelect = true;
-
     protected gameList = new GameList();
-    protected categorySelected = 0;
-
-    protected selectedGameId = 0;
-
-    @Prop({required: true, default: 0})
-    protected verticalSelect?: number;
-
     protected mame = new Mame();
+    protected selectedCategory: GameCategory|null = null;
+    protected selectedGameId: number = 0;
+    protected selectedGame: Game|null = null;
 
     public created() {
         this.gameList = this.$store.getters.gameList;
-
-        window.addEventListener('keydown', (e) => {
-            if (e.code === 'Enter') {
-                if (this.verticalSelect === 1) {
-                    if (this.blockVerticalSelect) {
-                        if (this.gameFromCurrentCategory[this.selectedGameId]) {
-                            this.mame.start(this.gameFromCurrentCategory[this.selectedGameId]);
-                        }
-                    }
-                    this.blockVerticalSelect = !this.blockVerticalSelect;
-                    this.$emit('blockVerticalSelect', this.blockVerticalSelect);
-                }
-            } else if (e.code === 'Escape') {
-                if (this.verticalSelect === 1) {
-                    this.blockVerticalSelect = false;
-                    this.$emit('blockVerticalSelect', false);
-                }
-            } else if (e.code === 'ArrowLeft') {
-                if (this.verticalSelect === 2 || (this.verticalSelect === 1 && this.blockVerticalSelect)) {
-                    this.selectedGameId = 0;
-                    this.categorySelected--;
-                }
-            } else if (e.code === 'ArrowRight') {
-                if (this.verticalSelect === 2 || (this.verticalSelect === 1 && this.blockVerticalSelect)) {
-                    this.selectedGameId = 0;
-                    this.categorySelected++;
-                }
-            } else if (e.code === 'ArrowUp') {
-                if (this.blockVerticalSelect) {
-                    this.selectedGameId = this.selectedGameId <= 0
-                        ? this.gameFromCurrentCategory.length - 1
-                        : this.selectedGameId - 1;
-                }
-            } else if (e.code === 'ArrowDown') {
-                if (this.blockVerticalSelect) {
-                    this.selectedGameId = this.selectedGameId >= this.gameFromCurrentCategory.length - 1
-                        ? 0
-                        : this.selectedGameId + 1;
-                }
-            }
-        });
-    }
-
-    @Watch('selectedGameId')
-    protected a() {
-        (this.$refs.gameList as HTMLElement).style.top = (-10 * this.selectedGameId) + '%';
+        this.mame = this.$store.getters.mame;
+        this.selectedCategory = this.gameList.getCategories()[0]; // Category ALL
+        this.selectedGame = this.selectedCategory.getGames()[0];
     }
 
     /**
-     *
+     * Called when categoryChange event is triggered on Categories component
+     * @param categoryId
      */
-    public get gameFromCurrentCategory() {
-        return this.gameList.getCategories().length ? this.gameList.getCategories()[0].getGames() : [];
+    public categoryChange(categoryId: number) {
+        this.selectedCategory = this.gameList.getCategories()[categoryId];
     }
 
-    public get selectedGame() {
-        return this.gameFromCurrentCategory[this.selectedGameId];
+    /**
+     * Called when gameChange event is triggered on Games component
+     * @param gameId
+     */
+    public gameChange(gameId: number) {
+        this.selectedGameId = gameId;
+        this.selectedGame = this.selectedCategory!.getGames()[gameId];
+        console.log(this.selectedGame);
     }
 }
 </script>
@@ -155,72 +97,4 @@ export default class Home extends Vue {
                 6px 12px 9px rgba(0, 0, 0, 1);
             transform: rotateX(15deg) rotateY(0deg) rotateZ(0deg);
         }
-
-    .gamesContainer{
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        position: relative;
-        /*z-index: 12;*/
-        filter: saturate(2);
-    }
-
-    .selectedGameBackground {
-        position: absolute;
-        left: 0;
-        top: 35%;
-        background: linear-gradient(to right, rgba(0, 30, 255, 0.25) 50%, transparent);
-        height: 30%;
-        width: 50%;
-    }
-
-    .games {
-        position: relative;
-        top: 35%;
-        height: 65%;
-        /*border: 1px solid red;*/
-        width: 50%;
-    }
-        .games ul {
-            position: absolute;
-            top: 0;
-            right: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            /*border: 1px solid yellow;*/
-            overflow: visible;
-            transition: top 0.3s ease;
-        }
-
-        .games ul li {
-            display: flex;
-            height: 10%;
-            width: 100%;
-            /*border: 1px solid green;*/
-            position: relative;
-            align-items: center;
-        }
-        .games ul li.selected {
-            height: 46%;
-            top: 0;
-        }
-
-        .games .marquee {
-            margin-left: 10%;
-            display: inline-block;
-            width: 35%;
-            height: 90%;
-            background-repeat: no-repeat;
-            background-image: url(../assets/default_marquee.jpg);
-            background-size: cover;
-            background-position: center;
-            border-radius: 5px;
-            box-shadow: 0 0 30px #000000;
-            transition: height 0.3s ease, width 0.3s ease, margin-left 0.3s ease;
-        }
-            .games ul li.selected .marquee {
-                width: 100%;
-                height: 80%;
-            }
 </style>
