8a7c57cb76bbf6cf54ea68ef26d6e6aebe7397a8
Tristan Payen <tristan.payen@infopro-digital.com>
2019-03-08 14:15:54 +0100

    Add name of rom as parameter

--- src/class/HiScore.class.ts
+++ src/class/HiScore.class.ts
@@ -1,15 +1,24 @@
 import {execFile} from 'child_process';
 import parse from 'csv-parse/lib/sync';
 import {join} from 'path';
+import Game from '@/class/Game.class';
 
 export default class HiScore {
 
-    public getHiscore(): Promise<any[] | string> {
+    public getHiscore(romName: string): Promise<any[] | string> {
         return new Promise((resolve, reject) => {
             const hi2txtPath = join(process.env.NODE_ENV === 'development'
                 ? './resources' : process.resourcesPath!, 'hi2txt');
-            execFile('java',
-                ['-jar', join(hi2txtPath, 'hi2txt.jar'), '-descr', join(hi2txtPath, 'hi2txt'), '-r', 'hi/asteroid.hi'],
+            execFile(
+                'java',
+                [
+                    '-jar',
+                    join(hi2txtPath, 'hi2txt.jar'),
+                    '-descr',
+                    join(hi2txtPath, 'hi2txt'),
+                    '-r',
+                    'hi/' + romName + '.hi',
+                ],
                 (error, stdout, stderr) => {
                     if (error) {
                         return reject(error);
