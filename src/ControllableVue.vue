6a372ccf07cebf4fda682bdcf8f0ea7c32c7a2f9
Tristan Payen <tristan.payen@infopro-digital.com>
2019-03-10 15:43:18 +0100

    Use one var

--- src/ControllableVue.vue
+++ src/ControllableVue.vue
@@ -3,19 +3,18 @@ import {Vue} from 'vue-property-decorator';
 
 export default abstract class ControllableVue extends Vue {
     protected keyPressed: {[key: string]: boolean} = {};
-    protected gamepadKeyPressed: {[key: string]: boolean} = {};
 
     protected onKeydown(keyActions: (e: Event, isGamepad: boolean) => void) {
         window.addEventListener('keydown', (e: KeyboardEvent) => {
             if (!this.keyPressed[e.key]) {
-                keyActions(e as KeyboardEvent, false);
                 this.keyPressed[e.key] = true;
+                keyActions(e as KeyboardEvent, false);
             }
         });
         window.addEventListener('gamepadKeydown', (e) => {
-            if (!this.gamepadKeyPressed[(e as CustomEvent).detail.key]) {
+            if (!this.keyPressed[(e as CustomEvent).detail.key]) {
                 keyActions(e, true);
-                this.gamepadKeyPressed[(e as CustomEvent).detail.key] = true;
+                this.keyPressed[(e as CustomEvent).detail.key] = true;
             }
         });
     }
@@ -28,9 +27,9 @@ export default abstract class ControllableVue extends Vue {
             }
         });
         window.addEventListener('gamepadKeyup', (e) => {
-            if (this.gamepadKeyPressed[(e as CustomEvent).detail.key]) {
+            if (this.keyPressed[(e as CustomEvent).detail.key]) {
                 keyActions(e, true);
-                this.gamepadKeyPressed[(e as CustomEvent).detail.key] = false;
+                this.keyPressed[(e as CustomEvent).detail.key] = false;
             }
         });
     }
