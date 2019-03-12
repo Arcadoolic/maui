6478dfe907c44fb3792007ccb0bf22397fc86f22
Tristan Payen <tristan.payen@infopro-digital.com>
2019-03-12 12:30:04 +0100

    Register init route

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Init from './views/Init.vue';
 
 Vue.use(Router);
 
@@ -8,6 +9,11 @@ export default new Router({
     routes: [
         {
             path: '/',
+            name: 'init',
+            component: Init,
+        },
+        {
+            path: '/home',
             name: 'home',
             component: Home,
         },
