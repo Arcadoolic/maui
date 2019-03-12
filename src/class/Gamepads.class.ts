ae3417ca7a137b99f7684097f20a14d32d493760
Tristan Payen <tristan.payen@infopro-digital.com>
2019-03-12 17:54:51 +0100

    Method to stop gamepads events

--- src/class/Gamepads.class.ts
+++ src/class/Gamepads.class.ts
@@ -16,8 +16,9 @@ export default class Gamepads {
             this.emitGamepadCountUpdate();
         });
 
-        window.addEventListener('gampaddisconnected', (e) => {
+        window.addEventListener('gamepaddisconnected', (e) => {
             this.gamepadCount--;
+            this.stopGamepadListeners((e as GamepadEvent).gamepad, (e as GamepadEvent).gamepad.index);
             if (!this.gamepadCount) {
                 this.stopGamepadsListeners();
             }
@@ -34,14 +35,15 @@ export default class Gamepads {
     }
 
     public static startGamepadListeners() {
-        for (const gamepadsKey in navigator.getGamepads()) {
-            if (!gamepadsKey) {
+        for (const navigatorGamepadsKey in navigator.getGamepads()) {
+            if (!navigatorGamepadsKey) {
                 continue;
             }
-            const gamepad: Gamepad|null = navigator.getGamepads()[gamepadsKey];
+            const gamepad: Gamepad|null = navigator.getGamepads()[navigatorGamepadsKey];
             if (!gamepad || !gamepad.connected || !gamepad.buttons) {
                 continue;
             }
+            const gamepadsKey = gamepad.index;
 
             if (!this.gamepadKeyPressed[gamepadsKey]) {
                 this.gamepadKeyPressed[gamepadsKey] = {axes: [], buttons: []};
@@ -112,10 +114,52 @@ export default class Gamepads {
         this.animationFrameRequest = requestAnimationFrame(this.startGamepadListeners.bind(this));
     }
 
+    /**
+     * Stop gamepads loop
+     */
     public static stopGamepadsListeners() {
         if (this.animationFrameRequest) {
             cancelAnimationFrame(this.animationFrameRequest);
         }
-        // TODO : Send allow gamepadKeyup of pressed buttons
+        if (!navigator.getGamepads()) {
+            return;
+        }
+        for (let gamepadIndex = 0; gamepadIndex < navigator.getGamepads().length; gamepadIndex++) {
+            const gamepad = navigator.getGamepads()[gamepadIndex];
+            if (!gamepad) {
+                continue;
+            }
+            return this.stopGamepadListeners(gamepad, gamepadIndex);
+        }
+    }
+
+    /**
+     * Send gamepadKeyup on all pressed buttons
+     * @param gamepad
+     * @param gamepadIndex
+     */
+    public static stopGamepadListeners(gamepad: Gamepad, gamepadIndex: number) {
+        const mapping = this.controllerMapping[gamepad.mapping || gamepad.id];
+        if (!mapping || !this.gamepadKeyPressed[gamepadIndex]) {
+            return;
+        }
+        for (let axesIndex = 0; axesIndex < this.gamepadKeyPressed[gamepadIndex].axes.length; axesIndex++) {
+            const axe = this.gamepadKeyPressed[gamepadIndex].axes[axesIndex];
+            if (axe && axe.wasPressed) {
+                window.dispatchEvent(new CustomEvent(
+                    'gamepadKeyup',
+                    { detail: {key: axe.lastPressedKey, value: 0}},
+                ));
+            }
+        }
+        for (let buttonIndex = 0; buttonIndex < this.gamepadKeyPressed[gamepadIndex].buttons.length; buttonIndex++) {
+            if (this.gamepadKeyPressed[gamepadIndex].buttons[buttonIndex]) {
+                window.dispatchEvent(new CustomEvent(
+                    'gamepadKeyup',
+                    { detail: {key: mapping.buttons[buttonIndex], value: 0}},
+                ));
+            }
+        }
+        delete this.gamepadKeyPressed[gamepadIndex];
     }
 }
