1181c85511604c1a1102c85969aa593593de7942
Tristan Payen <tristan.payen@infopro-digital.com>
2019-03-12 14:06:45 +0100

    Remove all logic from this view

--- src/App.vue
+++ src/App.vue
@@ -145,33 +145,4 @@ export default class App extends Vue {
         font-size: 3vw;
         color: red;
     }
-
-    .controllers {
-        display: block;
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        color: #fff513;
-        font-size: 0.6vw;
-        text-align: center;
-        padding: 1% 2%;
-        background: linear-gradient(to top, rgba(0, 30, 255, 0.25) 50%, transparent);
-        width: 15%;
-        perspective: 460px; /** TODO : Perspective not workinmg */
-        perspective-origin: 50% 50%;
-        text-shadow:
-                0 0 30px rgba(237, 106, 10, 0.8),
-                0 3px 0 rgb(255, 81, 0),
-                0 5px 20px rgba(255, 81, 0, 0.5),
-                0 6px 5px rgba(242, 0, 10, 0.7),
-                0 12px 16px rgba(0, 0, 0, 1),
-                6px 12px 9px rgba(0, 0, 0, 1);
-        transform: rotateX(15deg) rotateY(0deg) rotateZ(0deg);
-        filter: saturate(1.3);
-    }
-        .controllers small {
-            display: block;
-            padding: 10%;
-            line-height: 150%;
-        }
 </style>
