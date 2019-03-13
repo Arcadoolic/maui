febe90742f38304d86b4a83f799f85c92a981f2e
Tristan Payen <tristan.payen@infopro-digital.com>
2019-03-13 14:11:46 +0100

    Better flyer animation

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -16,16 +16,18 @@
             </ul>
         </div>
 
-        <transition @before-enter="flyerAnimationBeforeEnter" @enter="flyerAnimationEnter" @leave="flyerAnimationLeave">
-            <div class="flyer" v-if="showFlyer
-                && selectedCategory.getGames()[selectedGameId] && selectedCategory.getGames()[selectedGameId].flyer"
-                :style="{backgroundImage: 'url(' + selectedCategory.getGames()[selectedGameId].flyer + ')'}"></div>
-        </transition>
+        <div class="flyer-container">
+            <transition @before-enter="flyerAnimationBeforeEnter" @enter="flyerAnimationEnter" @leave="flyerAnimationLeave">
+                <div class="flyer" v-if="showFlyer
+                    && selectedCategory.getGames()[selectedGameId] && selectedCategory.getGames()[selectedGameId].flyer"
+                    :style="{backgroundImage: 'url(' + this.flyerImage + ')'}"></div>
+            </transition>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import {Vue, Component, Prop, Watch} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 import GameList from '@/class/GameList.class';
 import Mame from '@/class/Mame.class';
 import GameCategory from '@/class/GameCategory.class';
@@ -34,37 +36,27 @@ import HiScore from '@/class/HiScore.class';
 import ControllableVue from '@/ControllableVue.vue';
 import Game from '@/class/Game.class';
 import Velocity from 'velocity-animate';
-import {remote} from 'electron'
+import {remote} from 'electron';
 
 @Component
 export default class Games extends ControllableVue {
+    protected timeouts: {[key: string]: any} = {
+        moveUp: 0 as any,
+        moveDown: 0 as any,
+        showFlyer: 0 as any,
+    };
 
-    protected get marqueeTransition() {
-        return 'height ' + this.transitionTime + 's ease, width ' + this.transitionTime + 's ease, margin-left '
-            + this.transitionTime + 's ease, margin-left 0.3s ease';
-    }
-
-    protected get marqueeStyle() {
-        return (game: Game, index: number) => {
-            return {
-                transition: this.marqueeTransition,
-                marginLeft: Math.max(9 - Math.abs(this.selectedGameId - index), 0) + '%',
-                backgroundImage: game.marquee ? 'url( ' +game.marquee +')' : false,
-            };
-        };
-    }
     protected gameList = new GameList();
     protected mame = new Mame();
 
     protected selectedGameId = 0;
 
-    protected moveUpTimeout: any = 0;
-    protected moveDownTimeout: any = 0;
-
     protected transitionTime = 0.3;
 
     protected showGames = false;
     protected showFlyer = true;
+
+    protected flyerImage: string|null = null;
 
     @Prop({required: true, type: GameCategory}) protected selectedCategory!: GameCategory;
 
@@ -72,6 +64,8 @@ export default class Games extends ControllableVue {
         /** Init vars */
         this.gameList = this.$store.getters.gameList;
         this.mame = this.$store.getters.mame;
+
+        this.flyerImage = this.selectedCategory.getGames()[0].flyer;
 
         this.onKeydown((e: Event, isGamepad: boolean) => {
             const key = (isGamepad) ? (e as CustomEvent).detail.key : (e as KeyboardEvent).key;
@@ -95,14 +89,12 @@ export default class Games extends ControllableVue {
             const key = (isGamepad) ? (e as CustomEvent).detail.key : (e as KeyboardEvent).key;
             switch (key) {
                 case 'ArrowUp':
-                    clearTimeout(this.moveUpTimeout);
-                    this.moveUpTimeout = 0;
-                    this.showFlyer = true;
+                    clearTimeout(this.timeouts.moveUp);
+                    this.timeouts.moveUp = 0;
                     break;
                 case 'ArrowDown':
-                    clearTimeout(this.moveDownTimeout);
-                    this.moveDownTimeout = 0;
-                    this.showFlyer = true;
+                    clearTimeout(this.timeouts.moveDown);
+                    this.timeouts.moveDown = 0;
                     break;
             }
         });
@@ -112,6 +104,10 @@ export default class Games extends ControllableVue {
         this.showGames = true;
     }
 
+    /***
+     * ANIMTATIONS AND STYLES
+     ***/
+
     public gamesAnimationBeforeEnter(el: HTMLElement) {
         el.style.marginLeft = '-100%';
     }
@@ -119,43 +115,56 @@ export default class Games extends ControllableVue {
     public gamesAnimationEnter(el: HTMLElement, done: () => void) {
         Velocity(el, {marginLeft: 0}, {
             duration: Math.random() * (400 - 600) + 400,
+            easing: 'ease-in',
             complete: done,
-            easing: 'ease-in'
         });
     }
 
     public flyerAnimationBeforeEnter(el: HTMLElement) {
-        el.style.right = '-100%';
+        el.style.marginLeft = '100%';
     }
 
     public flyerAnimationEnter(el: HTMLElement, done: () => void) {
-        Velocity(el, {right: 0}, {
+        console.log(el);
+        Velocity(el, {marginLeft: 0}, {
             duration: 300,
+            easing: 'ease-out',
             complete: done,
-            easing: 'ease-out'
         });
     }
 
     public flyerAnimationLeave(el: HTMLElement, done: () => void) {
-        Velocity(el, {right: '-100%'}, {
+        Velocity(el, {marginLeft: '100%'}, {
             duration: 300,
+            easing: 'ease-out',
             complete: done,
-            easing: 'ease-out'
         });
+    }
+
+    protected get marqueeTransition() {
+        return 'height ' + this.transitionTime + 's ease, width ' + this.transitionTime + 's ease, margin-left '
+            + this.transitionTime + 's ease, margin-left 0.3s ease';
+    }
+
+    protected get marqueeStyle() {
+        return (game: Game, index: number) => {
+            return {
+                transition: this.marqueeTransition,
+                marginLeft: Math.max(9 - Math.abs(this.selectedGameId - index), 0) + '%',
+                backgroundImage: game.marquee ? 'url( ' + game.marquee + ')' : false,
+            };
+        };
     }
 
     /**
      * Called on move up
      */
     protected moveUp(speed: number, incrementer = 1) {
-        let newSpeed = speed;
-        newSpeed = speed - Math.pow(1.5, incrementer);
+        const newSpeed = this.getGameAnimationSpeed(speed, incrementer);
         incrementer += 1;
-        if (newSpeed < 200) {
-            newSpeed = 200;
-        }
-        if (speed < 300) {
-            this.showFlyer = false;
+        this.showFlyer = false;
+        if (speed > 250) {
+            this.animateFlyers();
         }
         this.transitionTime = speed / 1000;
         return () => {
@@ -163,23 +172,21 @@ export default class Games extends ControllableVue {
                 this.selectedCategory.getGames().length - 1 : this.selectedGameId - 1;
             this.updateGamesPosition();
             this.emitGameChange();
-            this.moveUpTimeout = setTimeout(this.moveUp(newSpeed, incrementer), newSpeed);
+            this.timeouts.moveUp = setTimeout(this.moveUp(newSpeed, incrementer), newSpeed);
             return;
         };
     }
-
 
     /**
      * Called on move down
      */
     protected moveDown(speed: number, incrementer = 1) {
-        let newSpeed = speed;
-        newSpeed = speed - Math.pow(1.5, incrementer);
+        const newSpeed = this.getGameAnimationSpeed(speed, incrementer);
         incrementer += 1;
-        if (newSpeed < 200) {
-            newSpeed = 200;
-        }
-        if (speed < 300) {
+        this.showFlyer = false;
+        if (speed > 250) {
+            this.animateFlyers();
+        } else {
             this.showFlyer = false;
         }
         this.transitionTime = speed / 1000;
@@ -188,9 +195,26 @@ export default class Games extends ControllableVue {
                 0 : this.selectedGameId + 1;
             this.updateGamesPosition();
             this.emitGameChange();
-            this.moveDownTimeout = setTimeout(this.moveDown(newSpeed, incrementer), newSpeed);
+            this.timeouts.moveDown = setTimeout(this.moveDown(newSpeed, incrementer), newSpeed);
             return;
         };
+    }
+
+    protected getGameAnimationSpeed(previousSpeed: number, incrementer: number) {
+        let newSpeed = previousSpeed - Math.pow(1.5, incrementer);
+        if (newSpeed < 200) {
+            newSpeed = 200;
+        }
+        return newSpeed;
+    }
+
+    protected animateFlyers() {
+        this.showFlyer = false;
+        clearTimeout(this.timeouts.showFlyer);
+        this.timeouts.showFlyer = setTimeout(() => {
+            this.flyerImage = this.selectedCategory.getGames()[this.selectedGameId].flyer;
+            this.showFlyer = true;
+        }, 300);
     }
 
     /**
@@ -201,7 +225,7 @@ export default class Games extends ControllableVue {
         this.mame.start(selectedGame).then(
             (process: ChildProcess|void) => {
                 if (process) {
-                    process.on('close', (e) => {
+                    process.on('close', () => {
                         if (selectedGame.hasHiscore) {
                             (new HiScore()).getHiscore(selectedGame.romName).then(
                                 (hiscore: any) => {
@@ -228,8 +252,7 @@ export default class Games extends ControllableVue {
      * If selectedCategories change, reset current game to 0 and emit event
      */
     @Watch('selectedCategory')
-    protected watchSelectedCategory(val: GameCategory, oldVal: GameCategory) {
-        console.log('Hello');
+    protected watchSelectedCategory() {
         this.selectedGameId = 0;
         this.emitGameChange();
         this.updateGamesPosition();
@@ -311,12 +334,16 @@ export default class Games extends ControllableVue {
         height: 80%;
     }
 
-    .flyer {
+    .flyer-container {
         position: absolute;
         right: -3%;
         top: -5%;
         bottom: -5%;
         width: 40%;
-        transform: rotateZ(-4deg);
     }
+        .flyer {
+            width: 100%;
+            height: 100%;
+            transform: rotateZ(-4deg);
+        }
 </style>
