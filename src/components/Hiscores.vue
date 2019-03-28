74396321760f40026cae8ecb849a466c2d91e41b
Tristan Payen <tristan.payen@infopro-digital.com>
2019-03-28 13:56:06 +0100

    Vue to display hiscores

---
+++ src/components/Hiscores.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="hiscore of game.hiscores.classic[0]">
+                {{hiscore.RANK}} - {{hiscore.NAME.trim() != '' ? hiscore.NAME : '???'}} - {{hiscore.SCORE}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    import ControllableVue from "@/ControllableVue.vue";
+    import {Component, Prop} from "vue-property-decorator";
+    import Game from "@/class/Game.class";
+
+    @Component
+    export default class Hiscores extends ControllableVue {
+        @Prop({required: true, type: Game}) protected game!: Game;
+
+        public mounted() {
+            console.log(this.game.shortname);
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        position: absolute;
+        background-color: red;
+        color: white;
+        right: 10%;
+        bottom: 0;
+        width: 30%;
+        height: 30%;
+    }
+</style>
