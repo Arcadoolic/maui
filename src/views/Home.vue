904454e12e89191d346efdd0db4f40c8692b5005
Tristan Payen <tristan.payen@infopro-digital.com>
2019-04-03 15:25:44 +0200

    Button to show/hide hiscores

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,14 +8,17 @@
         <Games :selectedCategory="selectedCategory" @gameChange="gameChange"></Games>
         <Categories @categoryChange="categoryChange"></Categories>
         <GamepadsComponent></GamepadsComponent>
-        <Hiscores :game="selectedGame" v-if="selectedGame.hasHiscore"></Hiscores>
+
+        <transition name="slide">
+            <Hiscores :game="selectedGame" v-if="selectedGame.hasHiscore && showHiscores"></Hiscores>
+        </transition>
     </span>
     <!--<button v-if="mame.isGameOn" @click.prevent="mame.stop()">Kill</button>-->
 
 </template>
 
 <script lang="ts">
-import {Vue, Component} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
 import GameList from '@/class/GameList.class';
 import Categories from '@/components/Categories.vue';
 import Mame from '@/class/Mame.class';
@@ -25,6 +28,7 @@ import Game from '@/class/Game.class';
 import Gamepads from '@/class/Gamepads.class';
 import GamepadsComponent from '@/components/Gamepads.vue';
 import Hiscores from '@/components/Hiscores.vue';
+import ControllableVue from '@/ControllableVue.vue';
 
 @Component({
     components: {
@@ -34,18 +38,34 @@ import Hiscores from '@/components/Hiscores.vue';
         Hiscores,
     },
 })
-export default class Home extends Vue {
+export default class Home extends ControllableVue {
     protected gameList = new GameList();
     protected mame = new Mame();
     protected selectedCategory: GameCategory|null = null;
     protected selectedGameId: number = 0;
     protected selectedGame: Game|null = null;
+    protected showHiscores: boolean = false;
 
     public created() {
         this.gameList = this.$store.getters.gameList;
         this.mame = this.$store.getters.mame;
         this.selectedCategory = this.gameList.getCategories()[0]; // Category ALL
         this.selectedGame = this.selectedCategory.getGames()[0];
+
+        this.onKeydown((e: Event, isGamepad: boolean) => {
+            const key = (isGamepad) ? (e as CustomEvent).detail.key : (e as KeyboardEvent).code;
+            console.log(key);
+            switch (key) {
+                case 'Space':
+                    this.showHiscores = !this.showHiscores;
+                    break;
+            }
+        });
+
+        this.onKeyup((e: Event, isGamepad: boolean) => {
+            const key = (isGamepad) ? (e as CustomEvent).detail.key : (e as KeyboardEvent).code;
+        });
+
         Gamepads.init();
     }
 
@@ -97,4 +117,15 @@ export default class Home extends Vue {
             line-height: 3em;
             font-size: 1vw;
         }
+
+    .slide-leave-active {
+        transition: margin-bottom .3s ease-in 0s;
+    }
+    .slide-enter-active {
+        transition: margin-bottom .3s ease-out 0s;
+    }
+
+    .slide-enter, .slide-leave-to{
+        margin-bottom: -100%;
+    }
 </style>
