cf9ba52c3a22157019317492f6875ad91c347012
Tristan Payen <tristan.payen@infopro-digital.com>
2019-04-05 17:14:01 +0200

    Try to optimize and some css

--- src/views/Init.vue
+++ src/views/Init.vue
@@ -1,8 +1,5 @@
 <template>
-    <div>
-        <p>Starting Arcade !</p>
-        <p>{{msg}}</p>
-    </div>
+    <div></div>
 </template>
 
 <script lang="ts">
@@ -18,76 +15,75 @@ import {ipcRenderer} from 'electron';
 export default class Init extends Vue {
     protected msg: string = 'Chargement';
 
-    public mounted() {
-        setTimeout(() => {
-            this.init().then(
-                () => {
-                    this.$store.commit('isInit');
-                    ipcRenderer.send('init-end');
-                    this.$router.push({name: 'home'});
-                },
-                (error) => {
-                    console.error(error);
-                },
-            );
-        }, 100);
+    public async mounted() {
+        this.init();
     }
 
-    protected init() {
-        return new Promise((resolve, reject) => {
-            try {
-                const config: Config = this.$store.getters.config;
-                const mame = this.$store.getters.mame;
-                const gameList = this.$store.getters.gameList;
+    protected async init() {
+        try {
+            const config: Config = this.$store.getters.config;
+            const mame = this.$store.getters.mame;
+            const gameList = this.$store.getters.gameList;
 
-                this.msg = 'Loading configuration file';
-                config.load();
-                this.msg = 'Loading MAME configuration files';
-                mame.init(config.mameIniPath);
-                this.msg = 'Loading games';
-                gameList.init(config.gamesJsonPath);
+            console.log('Loading configuration file');
+            config.load();
+            console.log('Loading MAME configuration files');
+            mame.init(config.mameIniPath);
+            console.log('Loading games');
+            gameList.init(config.gamesJsonPath);
 
-                const hiscore = new HiscoreService(config, mame.mameUiPath);
-                this.$store.commit('setHiscore', hiscore);
+            const hiscore = new HiscoreService(config, mame.mameUiPath);
+            this.$store.commit('setHiscore', hiscore);
 
-                const gameService = new GameService(config, mame, gameList, hiscore);
-                this.msg = 'Updating games';
-                gameService.refreshGameDir();
-                gameList.init(config.gamesJsonPath);
-                this.msg = 'Loading marquees';
-                gameService.loadGamesMarquee();
-                this.msg = 'Loading flyers';
-                gameService.loadGamesFlyers();
-                this.msg = 'Load Hiscores';
-                gameService.loadHiscores().then(() => {
-                        this.msg = 'Load players';
-                        const players = new Players(config.faceyourmangaPath);
-                        this.$store.commit('setPlayers', players);
-                        players.init();
+            const gameService = new GameService(config, mame, gameList, hiscore);
+            console.log('Updating games');
+            gameService.refreshGameDir();
+            gameList.init(config.gamesJsonPath);
+            console.log('Loading marquees');
+            gameService.loadGamesMarquee();
+            console.log('Loading flyers');
+            gameService.loadGamesFlyers();
+            console.log('Load Hiscores');
+            gameService.loadHiscores().then(() => {
+                    console.log('Load players');
+                    const players = new Players(config.faceyourmangaPath);
+                    this.$store.commit('setPlayers', players);
+                    players.init();
 
-                        // DB
-                        // TODO : Refacto
-                        const db = new IPDDatabase(config);
-                        this.$store.commit('setDb', db);
-                        this.msg = 'Init database';
-                        db.connect().then(
-                            () => {
-                                resolve();
-                            },
-                            (err) => {
-                                reject(err);
-                            },
-                        );
-                });
-            } catch (e) {
-                reject(e.toString());
-            }
-        });
+                    // DB
+                    // TODO : Refacto
+                    const db = new IPDDatabase(config);
+                    this.$store.commit('setDb', db);
+                    console.log('Init database');
+                    db.connect().then(
+                        () => {
+                            console.log('Initialisation done');
+                            this.$store.commit('isInit');
+                            ipcRenderer.send('init-end');
+                            this.$router.push({name: 'home'});
+                        },
+                        (err) => {
+                            console.error(err);
+                        },
+                    );
+            });
+        } catch (e) {
+            console.error(e.toString());
+        }
     }
 }
 
 </script>
 
 <style scoped>
-    p { color: red }
+    div {
+        display: block;
+        width: 100%;
+        height: 100%;
+        background-color: #000000;
+        background-image:  url(../assets/splash_screen_arcade.png);
+        background-size: cover;
+        background-repeat: repeat;
+        background-position: 0 0;
+    }
 </style>
