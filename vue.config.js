c256c50854ab78c29a7a568ae1955be587ae797f
Tristan Payen <tristan.payen@infopro-digital.com>
2019-05-25 01:40:46 +0200

    Set sqlite3 and sequelize as externals

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     pluginOptions: {
         electronBuilder: {
+            externals: ['sqlite3', 'sequelize'],
             builderOptions: {
                 appId: "mame.frontend",
                 productName: "Mame Frontend",
