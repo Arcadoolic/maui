568aadc1f9bbd529eabe246e7e31f95759a39562
Tristan Payen <tristan.payen@infopro-digital.com>
2019-05-27 00:43:31 +0200

    Add config route

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Router, {Route} from 'vue-router';
 import Home from './views/Home.vue';
 import Init from './views/Init.vue';
 import store from '@/store';
+import Config from '@/views/Config.vue';
 
 Vue.use(Router);
 
@@ -11,9 +12,6 @@ export default new Router({
         {
             path: '/',
             beforeEnter(to: Route, from: Route, next) {
-                if (store.getters.isInit) {
-                    return next({name: 'home'});
-                }
                 return next({name: 'init'});
             },
         },
@@ -38,6 +36,11 @@ export default new Router({
             //     }
             //     return next({name: 'init'});
             // },
+        },
+        {
+            path: '/config',
+            name: 'config',
+            component: Config
         },
     ],
 });
