d845aa276ead5c5c24b7b202ffd00667fc245a31
Tristan Payen <tristan.payen@infopro-digital.com>
2019-05-26 02:31:27 +0200

    Revert

--- src/views/Init.vue
+++ src/views/Init.vue
@@ -29,13 +29,6 @@ export default class Init extends Vue {
             (process.env.NODE_ENV === "development" ? '.' : userData),
             'mame-awesome-ui.sqlite'
         );
-        const configPath = join(
-            (process.env.NODE_ENV === "development" ? '.' : userData),
-            'mame-awesome-ui-config.json'
-        );
-
-        remote.dialog.showOpenDialog({ properties: ['openDirectory'] });
-
         this.$store.commit('initLogger', join(
             (process.env.NODE_ENV === "development" ? '.' : userData),
             'mame-awesome-ui.log'
@@ -50,11 +43,8 @@ export default class Init extends Vue {
         }));
 
         if (!existsSync(databasePath)) {
-            await this.installDatabase();
+            await this.install();
         }
-        // if (!existsSync(configPath)) {
-        //     await this.installConfig();
-        // }
 
         // Load games
     }
@@ -62,7 +52,7 @@ export default class Init extends Vue {
     /**
      * Init database
      */
-    protected async installDatabase() {
+    protected async install() {
         const db = this.$store.getters.database;
         await db.sync();
 
@@ -101,15 +91,6 @@ export default class Init extends Vue {
             {name: 'Utilities'},
             {name: 'Whac-A-Mole'},
         ]);
-    }
-
-    protected installConfig() {
-        const config = this.$store.getters.configuration;
-
-        if (!config.mameIniPath) {
-            remote.dialog.showOpenDialog({ properties: ['openDirectory'] })
-        }
-
     }
 
     protected async init() {
