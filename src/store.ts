22bc22f5ba262b3513bd17b00622e8f0616f701f
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-08 18:12:45 +0200

    Fix class init

--- src/store.ts
+++ src/store.ts
@@ -75,7 +75,7 @@ export default new Vuex.Store({
         initServices: (state) => {
             if (!state.isInit) {
                 state.mameService = new MameService(state.configuration);
-                state.gameService = new GameService(state.configuration, state.mameService);
+                state.gameService = new GameService(state.mameService);
                 state.userService = new UserService();
                 state.hiscoreService = new HiscoreService(state.mameService.iniPath, state.userService);
                 state.isInit = true;
