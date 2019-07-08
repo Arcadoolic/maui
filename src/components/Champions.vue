08cc2d5c6f6ce055d98b722925a882d22a5dc78e
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-08 18:12:14 +0200

    Get champions

--- src/components/Champions.vue
+++ src/components/Champions.vue
@@ -2,7 +2,8 @@
     <div class="champions">
         <div class="championsContainer" v-if="champions.length">
             <div v-for="(champion, index) of champions" class="champion" :style="{right: (index * 10) + '%'}">
-                <img :src="champion.icon" :alt="champion.name">
+                <!--<img :src="champion.icon" :alt="champion.name">-->
+                <p style="color: red">{{champion.user.pseudo_3}}</p>
             </div>
         </div>
         <img class="default" v-else src="../assets/hiscores.svg" alt="">
@@ -10,39 +11,30 @@
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
-import Game from '../class/Game.class';
+    import {Component, Prop, Vue, Watch} from "vue-property-decorator";
+    import Game from "@/model/Game.model";
+    import User from "@/model/User.model";
+    import {Sequelize} from 'sequelize-typescript';
 
-@Component
-export default class Champions extends Vue {
-    @Prop({required: true, type: Game}) protected game!: Game;
-    protected champions: Array<{name: string, icon: string}> = [];
+    @Component
+    export default class Champions extends Vue {
+        @Prop({required: true, type: Game})
+        protected game!: Game;
 
-    public mounted() {
-        this.onGameChange();
-    }
+        protected champions: Hiscore[] = [];
+        protected loading = true;
 
-    @Watch('game.hiscores')
-    public onGameChange() {
-        this.champions = [];
-        if (!this.game.hiscores || !this.game.hiscores.season.classic || !this.game.hiscores.season.classic[0]) {
-            return;
+        public async mounted() {
+            await this.onGameChange();
         }
-        const players = this.$store.getters.players;
-        const championsName: string[] = [];
-        for (const player of this.game.hiscores.season.classic[0] as any[]) {
-            if (this.champions.length >= 3) { break; }
-            if (players.playerExist(player.NAME) && championsName.indexOf(player.NAME) < 0) {
-                championsName.push(player.NAME);
-                this.champions.push({
-                    name: player.NAME,
-                    icon: players.getPlayerIcon(player.NAME),
-                });
-            }
+
+        @Watch("game")
+        public async onGameChange() {
+            this.loading = true;
+            this.champions = await this.game.$get('hiscores', {"include": [{model: User}], limit: 3, order: [['score', 'DESC']], group: ['hiscore.id_user']}) as Hiscore[];
+            this.loading = false;
         }
-        this.champions = this.champions.reverse();
     }
-}
 </script>
 
 <style scoped>
@@ -51,24 +43,30 @@ export default class Champions extends Vue {
         width: 100%;
         position: relative;
     }
+
     .champion {
         display: inline-block;
         height: 100%;
         right: 0;
         position: absolute;
     }
+
     .champion:nth-child(1) {
         z-index: 1;
     }
+
     .champion:nth-child(2) {
         z-index: 2;
     }
+
     .champion:nth-child(3) {
         z-index: 3;
     }
+
     img {
         height: 100%;
     }
+
     .default {
         position: absolute;
         right: 0;
