fed024cd2ac700da562c980844adf75564afe03b
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-10 15:45:33 +0200

    Add button to select avatars path

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -3,7 +3,8 @@
         <p>Config</p>
         <div>
             <p>{{mamePath || 'undefined'}}</p>
-            <button @click.prevent-="selectMamePath()">Select path</button>
+            <button @click.prevent-="selectMamePath()">Select mame path</button>
+            <button @click.prevent="selectAvatarsPath()">Select avatars path</button>
         </div>
         <button @click.prevent="save">Save</button>
     </div>
@@ -12,14 +13,15 @@
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
     import {remote} from 'electron';
-    import {sep} from 'path';
-    import {lstatSync} from 'fs';
+    import {sep, join} from 'path';
+    import {lstatSync, existsSync, mkdirSync} from 'fs';
     import Helpers from '@/class/Helpers.class';
 
     @Component
     export default class Config extends Vue {
         protected mamePath: string = '';
         protected mameBinaryName: string = '';
+        protected avatarsPath: string = '';
 
         public created() {
             remote.getCurrentWindow().setResizable(true);
@@ -29,6 +31,7 @@
 
             this.mamePath = this.config.mamePath;
             this.mameBinaryName = this.config.mameBinaryName;
+            this.avatarsPath = this.config.avatarsPath;
         }
 
         public get config() {
@@ -55,9 +58,23 @@
             }
         }
 
+        public async selectAvatarsPath() {
+            const avatarsDirPath = await remote.dialog.showOpenDialog({
+                title: 'Select avatars directory path',
+                properties: ['openDirectory', 'createDirectory', 'promptToCreate'],
+                defaultPath: Helpers.getUserDataPath(),
+            });
+
+            if (!existsSync(join(avatarsDirPath[0], 'mame-awesome-ui-avatars'))) {
+                mkdirSync(join(avatarsDirPath[0], 'mame-awesome-ui-avatars'));
+            }
+            this.avatarsPath = join(avatarsDirPath[0], 'mame-awesome-ui-avatars');
+        }
+
         public save() {
             this.config.mamePath = this.mamePath;
-            this.config._mameBinaryName = this.mameBinaryName;
+            this.config.mameBinaryName = this.mameBinaryName;
+            this.config.avatarsPath = this.avatarsPath;
             this.config.save();
             return this.$router.push({name: 'init'});
         }
