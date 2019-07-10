0ac1f4ccf0d958a2e03db9be93aece5aab70c28b
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-10 12:13:17 +0200

    Clean code and check if game as hi

--- src/components/Champions.vue
+++ src/components/Champions.vue
@@ -2,39 +2,40 @@
     <div class="champions">
         <div class="championsContainer" v-if="champions.length">
             <div v-for="(champion, index) of champions" class="champion" :style="{right: (index * 10) + '%'}">
-                <!--<img :src="champion.icon" :alt="champion.name">-->
-                <p style="color: red">{{champion.user.pseudo_3}}</p>
+                <img src="../assets/defaultPlayer.png" :alt="champion.name">
             </div>
         </div>
-        <img class="default" v-else src="../assets/hiscores.svg" alt="">
+        <img v-else class="default" src="../assets/hiscores.svg" alt="">
     </div>
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
-import Game from '@/model/Game.model';
-import User from '@/model/User.model';
-import {Sequelize} from 'sequelize-typescript';
+    import {Component, Prop, Vue, Watch} from 'vue-property-decorator';
+    import Game from '@/model/Game.model';
+    import User from '@/model/User.model';
 
-@Component
-export default class Champions extends Vue {
-    @Prop({required: true, type: Game})
-    protected game!: Game;
+    @Component
+    export default class Champions extends Vue {
+        @Prop({required: true, type: Game})
+        protected game!: Game;
 
-    protected champions: Hiscore[] = [];
-    protected loading = true;
+        protected champions: Hiscore[] = [];
+        protected loading = true;
 
-    public async mounted() {
-        await this.onGameChange();
+        public async mounted() {
+            await this.onGameChange();
+        }
+
+        @Watch('game')
+        public async onGameChange() {
+            this.loading = true;
+            this.champions = await this.game.$get(
+                'hiscores',
+                {include: [{model: User}], limit: 3, order: [['score', 'DESC']], group: ['hiscore.id_user']}
+            ) as Hiscore[] || [];
+            this.loading = false;
+        }
     }
-
-    @Watch('game')
-    public async onGameChange() {
-        this.loading = true;
-        this.champions = await this.game.$get('hiscores', {include: [{model: User}], limit: 3, order: [['score', 'DESC']], group: ['hiscore.id_user']}) as Hiscore[] || [];
-        this.loading = false;
-    }
-}
 </script>
 
 <style scoped>
