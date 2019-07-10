2a0ba74b9eb629c6a2be4bc8d292d2e13fdbc8a4
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-10 15:45:17 +0200

    Add avatarsPath'

--- src/class/Config.class.ts
+++ src/class/Config.class.ts
@@ -5,22 +5,12 @@ import {remote} from 'electron';
 import {execFile} from 'child_process';
 
 export default class Config {
+    public mamePath: string = '';
+    public mameBinaryName: string =  '';
+    public avatarsPath: string = '';
+
     protected configPath!: string;
     protected _configLoaded: boolean = false;
-
-    protected _mamePath: string = '';
-    protected _mameBinaryName: string =  '';
-
-    protected _defaultGamesJsonPath: string = './games';
-    protected _defaultHiscoresJsonPath: string = './hiscores';
-    protected _defaultFaceyourmangaPath: string = './faceyourmanga';
-    protected _defaultDb: ConnectionConfig = {host: 'localhost', user: 'root', password: '', database: 'mame'};
-
-    protected _mameIniPath?: string;
-    protected _gamesJsonPath?: string;
-    protected _hiscoresJsonPath?: string;
-    protected _faceyourmangaPath?: string;
-    protected _db?: ConnectionConfig;
 
     public constructor() {
         this.configPath = join(
@@ -37,14 +27,10 @@ export default class Config {
         if (existsSync(this.configPath)) {
             const configFile = JSON.parse(readFileSync(this.configPath, 'utf8'));
 
-            this._mamePath = configFile.mamePath;
-            this._mameBinaryName = configFile.mameBinaryName;
+            this.mamePath = configFile.mamePath;
+            this.mameBinaryName = configFile.mameBinaryName;
+            this.avatarsPath = configFile.avatarsPath;
 
-            // this._mameIniPath = configFile.mameIniPath;
-            // this._gamesJsonPath = configFile.gamesJsonPath;
-            // this._hiscoresJsonPath = configFile.hiscoresJsonPath;
-            // this._faceyourmangaPath = configFile.faceyourmangaPath;
-            // this._db = configFile.db;
             this._configLoaded = true;
             return true;
         }
@@ -59,63 +45,14 @@ export default class Config {
                 'mame-awesome-ui-config.json',
             ),
             JSON.stringify({
-                mamePath: this._mamePath,
-                mameBinaryName: this._mameBinaryName,
+                mamePath: this.mamePath,
+                mameBinaryName: this.mameBinaryName,
+                avatarsPath: this.avatarsPath,
             }),
         );
     }
 
     public loaded() {
         return this._configLoaded;
-    }
-
-    public get mamePath(): string {
-        return this._mamePath;
-    }
-    public set mamePath(val: string) {
-        this._mamePath = val;
-    }
-
-    public get mameBinaryName(): string {
-        return this._mameBinaryName;
-    }
-    public set mameBinaryName(val: string) {
-        this._mameBinaryName = val;
-    }
-
-    /**
-     * @return string
-     */
-    public get mameIniPath(): string|undefined {
-        return this._mameIniPath;
-    }
-
-    public set mameIniPath(val: string|undefined) {
-        this._mameIniPath = val;
-    }
-
-    /**
-     * @return string
-     */
-    public get gamesJsonPath(): string {
-        return this._gamesJsonPath || this._defaultGamesJsonPath;
-    }
-
-    /**
-     * @return string
-     */
-    public get hiscoresJsonPath(): string {
-        return this._hiscoresJsonPath || this._defaultHiscoresJsonPath;
-    }
-
-    /**
-     * @return string
-     */
-    public get faceyourmangaPath(): string {
-        return this._faceyourmangaPath || this._defaultFaceyourmangaPath;
-    }
-
-    public get db(): ConnectionConfig {
-        return this._db || this._defaultDb;
     }
 }
