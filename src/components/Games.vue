795046d8cf5e149104d39a6608ab5225681e8e0e
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-11 11:16:25 +0200

    Emit game-quit on EventBus

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -3,16 +3,18 @@
         <div class="selectedGameBackground"></div>
         <div class="games">
             <ul ref="gameList">
-                <li v-for="(game, index) in games" :class="{selected: selectedGameIndex === index}">
-                    <div class="marquee"
-                         :style="{
-                            marginLeft: Math.max(9 - Math.abs(selectedGameIndex - index), 0) + '%',
-                            backgroundImage: getMarquee(game.romName)
-                        }"
-                    >
-                        <Champions v-if='game.hi' :game='game'></Champions>
-                    </div>
-                </li>
+                <transition-group name="game-list">
+                    <li v-for="(game, index) in games" :class="{selected: selectedGameIndex === index}" :key="index">
+                        <div class="marquee"
+                             :style="{
+                                marginLeft: Math.max(9 - Math.abs(selectedGameIndex - index), 0) + '%',
+                                backgroundImage: getMarquee(game.romName)
+                            }"
+                        >
+                            <Champions v-if='game.hi' :game='game'></Champions>
+                        </div>
+                    </li>
+                </transition-group>
             </ul>
         </div>
     </div>
