04ab75d7e634df68123f4e90129ba4a8304d8808
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-18 12:13:43 +0200

    Fix champions order

--- src/components/Champions.vue
+++ src/components/Champions.vue
@@ -19,6 +19,7 @@
     import {join} from 'path';
     import {format} from 'url';
     import {EventBus} from '@/EventBus';
+    import {Sequelize} from 'sequelize-typescript';
 
     @Component
     export default class Champions extends Vue {
@@ -44,8 +45,11 @@
             this.loading = true;
             this.champions = await this.game.$get(
                 'hiscores',
-                {include: [{model: User}], limit: 3, order: [['score', 'DESC']], group: ['user.id_user']},
+                {include: [{model: User}], attributes: {include: [[Sequelize.fn('MAX', Sequelize.col('score')), 'max_score']]}, limit: 3, order: [['score', 'DESC']], group: ['user.id_user']},
             ) as Hiscore[] || [];
+            if (this.game.id_game === 11) {
+                console.log(this.champions);
+            }
             this.champions = this.champions.reverse();
             this.loading = false;
         }
