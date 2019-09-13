1ac4a9585b2566dfbeb296a30b4e327f5f42deaf
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-13 11:47:53 +0200

    Extends from Controller

--- src/api/GameController.ts
+++ src/api/GameController.ts
@@ -1,15 +1,23 @@
 import {Request, Response} from 'express';
 import Game from '@/model/Game.model';
+import Controller from "@/api/Controller";
+import {BrowserWindow, IpcMain} from "electron";
 
-export default class GameController
+export default class GameController extends Controller
 {
-    public static getGames(request: Request, response: Response) {
+    constructor(userDataPath: string, ipcMain: IpcMain, window: BrowserWindow) {
+        super(userDataPath, ipcMain, window);
+        this.getGames = this.getGames.bind(this);
+        this.getGameById = this.getGameById.bind(this);
+    }
+
+    public getGames(request: Request, response: Response) {
         Game.findAll().then((games: Game[]) => {
             return response.json(games);
         });
     }
 
-    public static getGameById(request: Request, response: Response) {
+    public getGameById(request: Request, response: Response) {
         Game.findByPk(request.params.id).then((game: Game|null) => {
             if (game) {
                 return response.json(game);
