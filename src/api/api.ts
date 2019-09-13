93cfb1f61cdb650383d9e00d6db81b4cd905eec3
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-13 11:48:44 +0200

    Fix controller calls

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,25 +2,28 @@ import express from 'express';
 import Database from '@/class/Database.class';
 import UserController from '@/api/UserController';
 import GameController from '@/api/GameController';
+import IpcMain = Electron.IpcMain;
+import BrowserWindow = Electron.BrowserWindow;
 
-export default function(userDataPath: string) {
+export default function(userDataPath: string, ipcMain: IpcMain, window: BrowserWindow) {
     const api: express.Application = express();
 
     const db = new Database(userDataPath); // Init db
 
     // Users routes
-    api.get('/api/users', UserController.getUsers);
-    api.get('/api/user/:id_user([0-9]+)', UserController.getUserById);
-
-    api.get('/api/user/:id_user([0-9]+)/game/:id_game([0-9]+)/scores', UserController.getScoresByIdGame);
+    const userController = new UserController(userDataPath, ipcMain, window);
+    api.get('/api/users', userController.getUsers);
+    api.get('/api/user/:id_user([0-9]+)', userController.getUserById);
+    api.get('/api/user/:id_user([0-9]+)/game/:id_game([0-9]+)/scores', userController.getScoresByIdGame);
 
     // Games routes
-    api.get('/api/games', GameController.getGames);
-    api.get('/api/game/:id([0-9]+)', GameController.getGameById);
+    const gameController = new GameController(userDataPath, ipcMain, window);
+    api.get('/api/games', gameController.getGames);
+    api.get('/api/game/:id([0-9]+)', gameController.getGameById);
 
     api.use((req, res, next) => {
         if (!db.exist()) {
-            return res.send({
+            return res.end({
                 success: false,
                 message: 'Database do not exist'
             });
