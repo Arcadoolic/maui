184197d9b06316b8642862d92d7058dcb8554e7a
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-20 14:01:21 +0200

    Component to add a new user

--- src/components/userRegistration.vue
+++ src/components/userRegistration.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="modal-container">
-        <div class="modal-background"></div>
-        <div class="modal user-registration-success" v-if="success">User created</div>
-        <div class="modal user-registration" v-else>
+    <modal>
+        <div class="user-registration-success" v-if="success">User created</div>
+        <div class="user-registration" v-else>
             <p>Please select a username. It will be used to extract and display your hiscores.</p>
             <div class="letters">
                 <div class="first-letter" :class="{selected: selectedLetter === 0}">
@@ -18,16 +17,18 @@
             <div class="error" v-if="error">Username already used</div>
             <div class="validate">
                 Press
-                <span class="arcadeButton yellow"></span>
+                <span class="arcadeButton">
+                    <i class="fas fa-male"></i>
+                </span>
                 to validate
             </div>
             <div class="cancel">
                 Press
-                <span class="arcadeButton blue"></span>
+                <span class="arcadeButton rectangle"></span>
                 to cancel
             </div>
         </div>
-    </div>
+    </modal>
 </template>
 
 <script lang="ts">
@@ -35,14 +36,21 @@
     import ControllableVue from "../ControllableVue";
     import User from "@/model/User.model";
     import {ValidationError} from 'sequelize';
+    import Modal from "@/components/Modal.vue";
 
-    @Component
+    @Component({
+        components: {
+            Modal
+        }
+    })
     export default class UserRegistration extends ControllableVue {
         protected username: {[key: number]: string} = {0: 'A', 1: 'A', 2: 'A'};
         protected selectedLetter: number = 0;
         protected success: boolean = false;
         protected error: boolean = false;
         protected loading: boolean = false;
+
+        protected prisitine: boolean = true;
 
         public created() {
             this.onKeydown((e, isGamepad) => {
@@ -60,13 +68,14 @@
                     case 'ArrowRight':
                         this.nextSelectedLetter();
                         break;
-                    case 'Enter':
-                        this.nextSelectedLetter(true);
+                    case 'KeyP':
+                        this.addUser();
                         break;
                     case 'Space':
                         this.$emit('quit');
                         break;
                 }
+                this.prisitine = false;
             });
 
             this.onKeyup((e, isGamepad) => { });
@@ -88,13 +97,8 @@
 
         protected nextSelectedLetter(isEnter: boolean = false) {
             if (!this.loading) {
-                if (this.selectedLetter === 2 && isEnter) {
-                    this.addUser();
-                } else {
-                    this.selectedLetter = this.selectedLetter === 2 ? 0 : this.selectedLetter + 1;
-                }
+                this.selectedLetter = this.selectedLetter === 2 ? 0 : this.selectedLetter + 1;
             }
-
         }
 
         protected previousSelectedLetter() {
@@ -104,7 +108,7 @@
         }
 
         protected addUser() {
-            if (this.loading) {
+            if (this.loading || this.prisitine) {
                 return;
             }
             this.error = false;
@@ -131,44 +135,21 @@
 </script>
 
 <style scoped>
-    .modal-container {
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        display: block;
-        width: 100%;
-        height: 100%;
-    }
-    .modal-background {
-        width: 100%;
-        height: 100%;
-        background: rgba(0, 0, 0, 0.6);
-    }
-
-    .modal {
-        display: inline-block;
-        position: absolute;
-        background: red;
-        width: 640px;
-        height: 300px;
-        top: 50%;
-        left: 50%;
-        margin-left: -320px;
-        margin-top: -150px;
-        padding: 10px;
-    }
         .user-registration > p {
             display: block;
             width: 100%;
             font-size: 14px;
             text-align: center;
+            line-height: 20px;
         }
 
         .letters {
             width: 40%;
-            border: 2px solid yellow;
             border-radius: 3px;
             margin: 20px auto;
+            color: #fff513;
+            filter: saturate(1.3);
+            text-shadow: 0 0 30px rgba(237, 106, 10, 0.8), 0 3px 0 rgb(255, 81, 0);
         }
             .letters > div {
                 display: inline-block;
@@ -180,7 +161,12 @@
                 display: block;
             }
             .letters > div.selected {
-                color: blue;
+                text-shadow: 0 0 30px rgba(237, 106, 10, 0.8),
+                0 3px 0 rgb(255, 81, 0),
+                0 5px 20px rgba(255, 81, 0, 0.5),
+                0 6px 5px rgba(242, 0, 10, 0.7),
+                0 12px 16px rgba(0, 0, 0, 1),
+                6px 12px 9px rgba(0, 0, 0, 1);
             }
 
         .error {
@@ -209,13 +195,14 @@
             border-radius: 50%;
             box-shadow: #a7a700 1px 1px;
             margin: 0 15px;
+            background-color: snow;
+            color: black;
+            line-height: 30px;
+            text-align: center;
+            font-size: 20px;
         }
-            .arcadeButton.yellow {
-                background-color: yellow;
-                box-shadow: #a7a700 1px 1px;
-            }
-            .arcadeButton.blue {
-                background-color: blue;
-                box-shadow: darkblue 1px 1px;
+            .arcadeButton.rectangle {
+                border-radius: 0;
+                width: 50px;
             }
 </style>
